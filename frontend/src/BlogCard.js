c7514ce5b6d564239f9c72178a31b9c468402e48
Thammarit Phiwton <poomz180@gmail.com>
2022-06-12 13:46:51 +0700

    create the unconnected backend BlogCard component.

---
+++ frontend/src/BlogCard.js
@@ -0,0 +1,14 @@
+
+function BlogCard() {
+  return (
+    <div className="flex flex-col justify-between w-80 h-80 p-2 rounded-lg shadow-lg">
+      <div>
+        <h1 className="text-xl font-bold">Hello yo yo yo</h1>
+        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aperiam consequatur deserunt dignissimos dolore eius eum ex expedita, explicabo fuga fugit laboriosam molestiae perferendis quasi quibusdam quidem tempore vitae!</p>
+      </div>
+      <p>John Domogo</p>
+    </div>
+  );
+}
+
+export default BlogCard;
