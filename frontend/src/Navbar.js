32219799767621899946a5ea8b4d92005bd74ade
Thammarit Phiwton <poomz180@gmail.com>
2022-06-14 20:36:40 +0700

    change Navbar position to sticky

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,7 +2,7 @@ import SignIn from "./SignIn";
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center bg-black text-white p-2">
+    <div className="sticky top-0 flex justify-between items-center bg-black text-white p-2">
       <h1 className="text-2xl font-bold">SimpleBlog</h1>
       <SignIn/>
     </div>
