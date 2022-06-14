1480be27d85121976724b166bd342bf816495d61
Thammarit Phiwton <poomz180@gmail.com>
2022-06-14 13:31:13 +0700

    use google sign in instead of login/signup button

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,12 +1,10 @@
+import SignIn from "./SignIn";
 
 function Navbar() {
   return (
     <div className="flex justify-between items-center bg-black text-white p-2">
       <h1 className="text-2xl font-bold">SimpleBlog</h1>
-      <div className="flex gap-2 items-center">
-        <p>login</p>
-        <p className="border-solid border-2 border-white rounded p-0.5">sign up</p>
-      </div>
+      <SignIn/>
     </div>
   );
 }
