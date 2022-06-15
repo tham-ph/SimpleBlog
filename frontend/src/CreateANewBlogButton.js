7a79cc70e230768825051b3d484664214a0d333d
Thammarit Phiwton <poomz180@gmail.com>
2022-06-15 23:01:10 +0700

    link to path "/create" or Editor when click the button

--- frontend/src/CreateANewBlogButton.js
+++ frontend/src/CreateANewBlogButton.js
@@ -1,8 +1,8 @@
-
+import { Link } from "react-router-dom";
 const CreateANewBlogButton = () => {
   return (
     <button className="fixed bottom-4 right-4 bg-gray-500 text-white text-lg py-1 px-8 rounded-full hover:bg-black">
-      Create A New Blog
+      <Link to="/create">Create A New Blog</Link>
     </button>
   );
 }
