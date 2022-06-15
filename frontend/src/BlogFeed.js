6e2fbad88e604945961c0ef8515bb89212015499
Thammarit Phiwton <poomz180@gmail.com>
2022-06-15 22:45:30 +0700

    move CreateANewButton from App to BlogFeed

--- frontend/src/BlogFeed.js
+++ frontend/src/BlogFeed.js
@@ -1,8 +1,10 @@
 import BlogCard from "./BlogCard";
+import CreateANewBlogButton from "./CreateANewBlogButton";
 
 const BlogFeed = () => {
   return (
     <div className="flex flex-wrap justify-center gap-4 p-2">
+      <CreateANewBlogButton/>
       <BlogCard/>
       <BlogCard/>
       <BlogCard/>
