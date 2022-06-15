62e96221493f051133a2733315792f867b7f4404
Thammarit Phiwton <poomz180@gmail.com>
2022-06-15 21:24:50 +0700

    implement a CreateANewBlogButton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import BlogList from "./BlogList";
+import CreateANewBlogButton from "./CreateANewBlogButton";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar/>
       <BlogList/>
+      <CreateANewBlogButton/>
     </div>
   );
 }
---
+++ frontend/src/CreateANewBlogButton.js
@@ -0,0 +1,10 @@
+
+const CreateANewBlogButton = () => {
+  return (
+    <button className="fixed bottom-4 right-4 bg-gray-500 text-white text-lg py-1 px-8 rounded-full hover:bg-black">
+      Create A New Blog
+    </button>
+  );
+}
+
+export default CreateANewBlogButton;
