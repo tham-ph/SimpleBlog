a801f56724a93cb726f25486c4cbcf674cd3aadc
Thammarit Phiwton <poomz180@gmail.com>
2022-06-16 14:22:11 +0700

    link header to "/" path

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,9 +1,14 @@
 import UserControl from "./UserControl";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="sticky top-0 flex justify-between items-center bg-gray-500 text-white p-2">
-      <h1 className="text-2xl font-bold">SimpleBlog</h1>
+      <h1 className="text-2xl font-bold">
+        <Link to="/">
+          SimpleBlog
+        </Link>
+      </h1>
       <UserControl/>
     </div>
   );
