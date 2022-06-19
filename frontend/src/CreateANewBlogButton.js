e29e1b85c6474b5fe2208a4c20e5942b8820a0bb
Thammarit Phiwton <poomz180@gmail.com>
2022-06-19 21:11:25 +0700

    set conditions for preventing guest users create a new blog

--- frontend/src/CreateANewBlogButton.js
+++ frontend/src/CreateANewBlogButton.js
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom";
+import SignInAlertModal from "./SignInAlertModal";
+import {useContext} from "react";
+import {getUserDataContext} from "./App";
+
 const CreateANewBlogButton = () => {
-  return (
-    <button className="fixed bottom-4 right-4 bg-gray-500 text-white text-lg py-1 px-8 rounded-full hover:bg-black">
-      <Link to="/create">Create A New Blog</Link>
-    </button>
-  );
+  const {userData} = useContext(getUserDataContext());
+
+  let ret;
+  if (Object.keys(userData).length === 0) {
+    ret = (
+      <div>
+        <SignInAlertModal/>
+        <button className="fixed bottom-4 right-4 bg-gray-500 text-white text-lg py-1 px-8 rounded-full hover:bg-black"
+          onClick={() => {
+            document.getElementById("sign-in-alert-modal").classList.remove("hidden");
+          }}
+        >
+          Create A New Blog
+        </button>
+      </div>
+    );
+  } else {
+    ret = (
+      <div>
+        <button className="fixed bottom-4 right-4 bg-gray-500 text-white text-lg py-1 px-8 rounded-full hover:bg-black">
+          <Link to="/create">Create A New Blog</Link>
+        </button>
+      </div>
+    );
+  }
+  return ret;
 }
 
 export default CreateANewBlogButton;
