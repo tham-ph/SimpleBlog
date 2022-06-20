36eb33200a29005609b9680b88fcf1f82375395a
Thammarit Phiwton <poomz180@gmail.com>
2022-06-20 21:21:30 +0700

    implement changeUserName api

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,21 +4,41 @@ const signInWithGoogle = async(req, res) => {
   const {email, name, pictureURL} = req.body;
 
   let user = await userModel.findOne({email});
-  console.log(user);
 
   if (!user) {
-    console.log("pass")
     user = await userModel.create({email, name, pictureURL});
   }
-
   res.json({
-    _id: user._id,
+    id: user._id,
     email: user.email,
     name: user.name,
     pictureURL: user.pictureURL
   });
 }
 
+const changeUserName = async(req, res) => {
+  const {userId, newName} = req.body;
+
+  const user = await userModel.findById(userId);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  user.name = newName;
+  const updatedUser = await user.save();
+  // const updatedUser = await userModel.update({_id: userId}, user);
+
+  res.json({
+    id: updatedUser._id,
+    email: updatedUser.email,
+    name: updatedUser.name,
+    pictureURL: updatedUser.pictureURL
+  });
+}
+
 module.exports = {
   signInWithGoogle,
+  changeUserName
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const signInWithGoogle = require("../controller/userController").signInWithGoogle;
 
+const signInWithGoogle = require("../controller/userController").signInWithGoogle;
 router.post("/sign-in-with-google", signInWithGoogle);
+
+const changeUserName = require("../controller/userController").changeUserName;
+router.post("/change-user-name", changeUserName)
 
 module.exports = router;
