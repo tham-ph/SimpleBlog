6f9f7f23c75e506a0be8327c6775774ae393ff90
Thammarit Phiwton <poomz180@gmail.com>
2022-06-21 22:41:10 +0700

    change padding

--- frontend/src/SignInAlertModal.js
+++ frontend/src/SignInAlertModal.js
@@ -4,7 +4,7 @@ import UserSignIn from "./UserSignIn";
 const SignInAlertModal = () => {
   return (
     <Modal id={"sign-in-alert-modal"}>
-      <div className="flex flex-col items-center gap-4 text-lg font-bold text-gray-500 p-8">
+      <div className="flex flex-col items-center gap-4 text-lg font-bold text-gray-500 p-6">
         Please sign in before do anything further.
         <UserSignIn/>
       </div>
