df6a4a29debf8dec72de7b8123e0306ff1ccf091
thamph <poomz180@gmail.com>
2022-07-05 21:14:53 +0700

    fetch blog data from api

--- frontend/src/BlogCard.js
+++ frontend/src/BlogCard.js
@@ -1,10 +1,22 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-function BlogCard() {
+function BlogCard({blogData}) {
   return (
     <div className="flex flex-col justify-between w-80 h-80 p-2 rounded-lg shadow-lg">
       <div>
-        <h1 className="text-xl font-bold">Hello yo yo yo</h1>
-        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aperiam consequatur deserunt dignissimos dolore eius eum ex expedita, explicabo fuga fugit laboriosam molestiae perferendis quasi quibusdam quidem tempore vitae!</p>
+        <h1>
+          <ReactMarkdown className="prose" remarkPlugins={[remarkGfm]}>
+            {blogData.title}
+          </ReactMarkdown>
+        </h1>
+        <p className="opacity-50">
+          <ReactMarkdown className="prose" remarkPlugins={[remarkGfm]}>
+            {blogData.content.substring(0, 250)}
+          </ReactMarkdown>
+        </p>
+        {/*<h1 className="text-xl font-bold">Hello yo yo yo</h1>*/}
+        {/*<p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aperiam consequatur deserunt dignissimos dolore eius eum ex expedita, explicabo fuga fugit laboriosam molestiae perferendis quasi quibusdam quidem tempore vitae!</p>*/}
       </div>
       <p>John Domogo</p>
     </div>
--- frontend/src/BlogFeed.js
+++ frontend/src/BlogFeed.js
@@ -1,20 +1,27 @@
 import BlogCard from "./BlogCard";
 import CreateANewBlogButton from "./CreateANewBlogButton";
+import {useEffect, useState} from "react";
+import Axios from "axios";
 
 const BlogFeed = () => {
+  const [blogList, setBlogList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("api/blogs/").then(res => {
+      setBlogList(res.data);
+    }).catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  const blogMap = blogList.map(blogData => {
+    return <BlogCard blogData={blogData}/>;
+  });
+
   return (
     <div className="flex flex-wrap justify-center gap-4 p-2">
       <CreateANewBlogButton/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
+      {blogMap}
     </div>
   );
 }
